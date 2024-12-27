b03fc827e701c2af56a914ee6ec46b61d159049a
abdulsamadmsd <abdulsamadpak111@gmail.com>
2024-12-26 16:38:19 -0800

    just remove the checking section

--- digital.js
+++ digital.js
@@ -10,9 +10,9 @@ function updateClock() {
     if (h >= 12) {
         ampm = "PM";
     }
-    // if (h > 12) {
-    //     h = h - 12;
-    // }
+    if (h > 12) {
+        h = h - 12;
+    }
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
